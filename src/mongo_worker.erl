2c6dd4c3f06cbdaa6fd57680396648af870b1967
Hisham Ismail <mhishami@gmail.com>
2015-11-10 19:40:49 +0800

    removed pool

--- src/mongo_worker.erl
+++ src/mongo_worker.erl
@@ -15,7 +15,7 @@
 -export([terminate/2]).
 -export([code_change/3]).
 
--record(state, {pool}).
+-record(state, {}).
 
 %% API.
 %% ----------------------------------------------------------------------------
