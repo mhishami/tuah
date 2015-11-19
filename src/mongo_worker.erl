cae66c0bbe1daea14552ff5240ed562e28590df5
Hisham Ismail <mhishami@gmail.com>
2015-11-19 15:18:48 +0800

    Added match_group functions

--- src/mongo_worker.erl
+++ src/mongo_worker.erl
@@ -22,19 +22,22 @@
 -export([
     save/2,             %% save the records
     update/2,           %% update by new Doc
-    update/3,           %% update by selector with new doc
-    update/4,           %% update by selector with new doc, with args
-    find_one/2,         %% find first item by selector
-    find_one/3,         %% find first item by selector, with args - {projector, skip}
-    find/2,             %% find all items by selector
-    find/3,             %% find all items by selector, with args - {batchsize, skip, projector}
-    match/3,            %% find items by selector, sort
-    match/4,            %% find items by selector, sort, limit
-    match/5,            %% find items by selector, sort, skip, limit
-    delete/2,           %% delete all by selector
-    delete_one/2,       %% delete one by selector
-    count/2,            %% count by selector
-    count/3,            %% count by selector, limit
+    update/3,           %% update by match with new doc
+    update/4,           %% update by match with new doc, with args
+    find_one/2,         %% find first item by match
+    find_one/3,         %% find first item by match, with args - {projector, skip}
+    find/2,             %% find all items by match
+    find/3,             %% find all items by match, with args - {batchsize, skip, projector}
+    match/3,            %% find items by match, sort
+    match/4,            %% find items by match, sort, limit
+    match/5,            %% find items by match, sort, skip, limit
+    match_group/3,      %% find items by match, group
+    match_group/4,      %% find items by match, group, sort
+    match_group/5,      %% find items by match, project, group, sort
+    delete/2,           %% delete all by match
+    delete_one/2,       %% delete one by match
+    count/2,            %% count by match
+    count/3,            %% count by match, limit
     ensure_index/2      %% index collection by index spec - {key, name, unique, dropDups}
 ]).
 
@@ -47,27 +50,27 @@ update(Coll, Doc) when is_map(Doc) ->
     gen_server:call(?MODULE, {update, Coll, Doc}).
 
 -spec update(binary(), any(), any()) -> {ok, any()} | {error, any()}.
-update(Coll, Selector, Doc) when is_map(Doc) ->
-    gen_server:call(?MODULE, {update, Coll, Selector, Doc}).
+update(Coll, Match, Doc) when is_map(Doc) ->
+    gen_server:call(?MODULE, {update, Coll, Match, Doc}).
 
 -spec update(binary(), any(), any(), list()) -> {ok, any()} | {error, any()}.
-update(Coll, Selector, Doc, Args) when is_map(Doc) ->
-    gen_server:call(?MODULE, {update, Coll, Selector, Doc, Args}).
+update(Coll, Match, Doc, Args) when is_map(Doc) ->
+    gen_server:call(?MODULE, {update, Coll, Match, Doc, Args}).
 
 -spec find_one(binary(), any()) -> {ok, any()} | {error, any()}.
-find_one(Coll, Selector) ->
-    gen_server:call(?MODULE, {find_one, Coll, Selector}).
+find_one(Coll, Match) ->
+    gen_server:call(?MODULE, {find_one, Coll, Match}).
 
 % Example:
 % mongo_worker:find(<<"posts">>, {}, [{batchsize, 1}, {skip, 1}]).
 % 
 -spec find_one(binary(), any(), list()) -> {ok, any()} | {error, any()}.
-find_one(Coll, Selector, Args) ->
-    gen_server:call(?MODULE, {find_one, Coll, Selector, Args}).
+find_one(Coll, Match, Args) ->
+    gen_server:call(?MODULE, {find_one, Coll, Match, Args}).
 
 -spec find(binary(), any()) -> {ok, any()}.
-find(Coll, Selector) ->
-    gen_server:call(?MODULE, {find, Coll, Selector, []}).
+find(Coll, Match) ->
+    gen_server:call(?MODULE, {find, Coll, Match, []}).
 
 % Example
 % mongo_worker:find(<<"posts">>, {}, [{batchsize, 3}, {skip, 1}, 
@@ -78,36 +81,48 @@ find(Coll, Selector) ->
 % ]).
 
 -spec find(binary(), any(), list()) -> {ok, any()}.
-find(Coll, Selector, Args) ->
-    gen_server:call(?MODULE, {find, Coll, Selector, Args}).
+find(Coll, Match, Args) ->
+    gen_server:call(?MODULE, {find, Coll, Match, Args}).
 
 -spec match(binary(), any(), any()) -> {ok, any()}.
-match(Coll, Selector, Sort) ->
-    gen_server:call(?MODULE, {match, Coll, Selector, Sort}).
+match(Coll, Match, Sort) ->
+    gen_server:call(?MODULE, {match, Coll, Match, Sort}).
 
 -spec match(binary(), any(), any(), byte()) -> {ok, any()}.
-match(Coll, Selector, Sort, Limit) ->
-    gen_server:call(?MODULE, {match, Coll, Selector, Sort, Limit}).
+match(Coll, Match, Sort, Limit) ->
+    gen_server:call(?MODULE, {match, Coll, Match, Sort, Limit}).
 
 -spec match(binary(), any(), any(), byte(), byte()) -> {ok, any()}.
-match(Coll, Selector, Sort, Skip, Limit) ->
-    gen_server:call(?MODULE, {match, Coll, Selector, Sort, Skip, Limit}).
+match(Coll, Match, Sort, Skip, Limit) ->
+    gen_server:call(?MODULE, {match, Coll, Match, Sort, Skip, Limit}).
+
+-spec match_group(Coll::binary(), Match::any(), Group::any()) -> {ok, any()}.
+match_group(Coll, Match, Group) ->
+    gen_server:call(?MODULE, {match_group, Coll, Match, Group}).
+
+-spec match_group(Coll::binary(), Match::any(), Group::any(), Sort::any()) -> {ok, any()}.
+match_group(Coll, Match, Group, Sort) ->
+    gen_server:call(?MODULE, {match_group, Coll, Match, Group, Sort}).
+
+-spec match_group(Coll::binary(), Match::any(), Project::any(), Group::any(), Sort::any()) -> {ok, any()}.
+match_group(Coll, Match, Project, Group, Sort) ->
+    gen_server:call(?MODULE, {match_group, Coll, Match, Project, Group, Sort}).
 
 -spec delete(binary(), any()) -> {ok, any()} | {error, any()}.
-delete(Coll, Selector) ->
-    gen_server:call(?MODULE, {delete, Coll, Selector}).
+delete(Coll, Match) ->
+    gen_server:call(?MODULE, {delete, Coll, Match}).
 
 -spec delete_one(binary(), any()) -> {ok, any()} | {error, any()}.
-delete_one(Coll, Selector) ->
-    gen_server:call(?MODULE, {delete_one, Coll, Selector}).
+delete_one(Coll, Match) ->
+    gen_server:call(?MODULE, {delete_one, Coll, Match}).
 
 -spec count(binary(), any()) -> {ok, any()} | {error, any()}.
-count(Coll, Selector) ->
-    gen_server:call(?MODULE, {count, Coll, Selector, 0}).
+count(Coll, Match) ->
+    gen_server:call(?MODULE, {count, Coll, Match, 0}).
 
 -spec count(binary(), any(), byte()) -> {ok, any()} | {error, any()}.
-count(Coll, Selector, Limit) ->
-    gen_server:call(?MODULE, {count, Coll, Selector, Limit}).
+count(Coll, Match, Limit) ->
+    gen_server:call(?MODULE, {count, Coll, Match, Limit}).
 
 -spec ensure_index(binary(), any()) -> any().
 ensure_index(Coll, IndexSpec) ->
@@ -146,30 +161,30 @@ handle_call({update, Coll, Doc}, _From, #state{pool=Pool} = State) ->
             end),            
     {reply, Reply, State};
 
-handle_call({update, Coll, Selector, Doc}, _From, #state{pool=Pool} = State) ->
+handle_call({update, Coll, Match, Doc}, _From, #state{pool=Pool} = State) ->
     Reply = poolboy:transaction(Pool, 
             fun(Conn) ->
-                case catch mongo:update(Conn, Coll, Selector, Doc) of
+                case catch mongo:update(Conn, Coll, Match, Doc) of
                     {'EXIT', Error} -> {error, Error};
                     Else -> {ok, Else}
                 end
             end),            
     {reply, Reply, State};
 
-handle_call({update, Coll, Selector, Doc, Args}, _From, #state{pool=Pool} = State) ->
+handle_call({update, Coll, Match, Doc, Args}, _From, #state{pool=Pool} = State) ->
     Reply = poolboy:transaction(Pool, 
             fun(Conn) ->
-                case catch mongo:update(Conn, Coll, Selector, Doc, Args) of
+                case catch mongo:update(Conn, Coll, Match, Doc, Args) of
                     {'EXIT', Error} -> {error, Error};
                     Else -> {ok, Else}
                 end
             end),            
     {reply, Reply, State};
 
-handle_call({find_one, Coll, Selector}, _From, #state{pool=Pool} = State) ->    
+handle_call({find_one, Coll, Match}, _From, #state{pool=Pool} = State) ->    
     Res = poolboy:transaction(Pool, 
             fun(Conn) ->
-                mongo:find_one(Conn, Coll, Selector)
+                mongo:find_one(Conn, Coll, Match)
             end),
     Reply = case maps:size(Res) of
                 0 -> {error, not_found};
@@ -177,10 +192,10 @@ handle_call({find_one, Coll, Selector}, _From, #state{pool=Pool} = State) ->
             end,
     {reply, Reply, State};
 
-handle_call({find_one, Coll, Selector, Args}, _From, #state{pool=Pool} = State) ->    
+handle_call({find_one, Coll, Match, Args}, _From, #state{pool=Pool} = State) ->    
     Res = poolboy:transaction(Pool, 
             fun(Conn) ->
-                mongo:find_one(Conn, Coll, Selector, Args)
+                mongo:find_one(Conn, Coll, Match, Args)
             end),
     Reply = case maps:size(Res) of
                 0 -> {error, not_found};
@@ -192,34 +207,34 @@ handle_call({find_one, Coll, Selector, Args}, _From, #state{pool=Pool} = State) 
 %   {<<"title">>, #{<<"$regex">>  => <<"some*">>, <<"$options">> => <<"i">>}}, 
 %   [{projector, #{<<"grpid">> => 1, <<"title">> => 1, <<"author.fullname">> => 1}}]).
 % 
-handle_call({find, Coll, Selector, Args}, _From, #state{pool=Pool} = State) ->
+handle_call({find, Coll, Match, Args}, _From, #state{pool=Pool} = State) ->
     Res = poolboy:transaction(Pool,
             fun(Conn) ->
-                Cursor = mongo:find(Conn, Coll, Selector, Args),
+                Cursor = mongo:find(Conn, Coll, Match, Args),
                 Results = mc_cursor:rest(Cursor),
                 mc_cursor:close(Cursor),
                 Results
             end),
     {reply, {ok, Res}, State};
 
-handle_call({match, Coll, Selector, Sort}, _From, #state{pool=Pool} = State) ->
+handle_call({match, Coll, Match, Sort}, _From, #state{pool=Pool} = State) ->
     Res = poolboy:transaction(Pool,
             fun(Conn) ->
                 {true, #{<<"result">> := Results}} = mongo:command(Conn,
                     {<<"aggregate">>, Coll, <<"pipeline">>, [
-                        {<<"$match">>, Selector},
+                        {<<"$match">>, Match},
                         {<<"$sort">>, Sort}
                     ]}),
                 Results
             end),
     {reply, {ok, Res}, State};
 
-handle_call({match, Coll, Selector, Sort, Limit}, _From, #state{pool=Pool} = State) ->
+handle_call({match, Coll, Match, Sort, Limit}, _From, #state{pool=Pool} = State) ->
     Res = poolboy:transaction(Pool,
             fun(Conn) ->
                 {true, #{<<"result">> := Results}} = mongo:command(Conn,
                     {<<"aggregate">>, Coll, <<"pipeline">>, [
-                        {<<"$match">>, Selector},
+                        {<<"$match">>, Match},
                         {<<"$sort">>, Sort},
                         {<<"$limit">>, Limit}
                     ]}),
@@ -228,12 +243,12 @@ handle_call({match, Coll, Selector, Sort, Limit}, _From, #state{pool=Pool} = Sta
 
     {reply, {ok, Res}, State};
 
-handle_call({match, Coll, Selector, Sort, Skip, Limit}, _From, #state{pool=Pool} = State) ->
+handle_call({match, Coll, Match, Sort, Skip, Limit}, _From, #state{pool=Pool} = State) ->
     Res = poolboy:transaction(Pool,
             fun(Conn) ->
                 {true, #{<<"result">> := Results}} = mongo:command(Conn,
                     {<<"aggregate">>, Coll, <<"pipeline">>, [
-                        {<<"$match">>, Selector},
+                        {<<"$match">>, Match},
                         {<<"$sort">>, Sort},
                         {<<"$skip">>, Skip},
                         {<<"$limit">>, Limit}
@@ -243,24 +258,63 @@ handle_call({match, Coll, Selector, Sort, Skip, Limit}, _From, #state{pool=Pool}
 
     {reply, {ok, Res}, State};
 
-handle_call({delete, Coll, Selector}, _From, #state{pool=Pool} = State) ->    
+handle_call({match_group, Coll, Match, Group}, _From, #state{pool=Pool} = State) ->
+    Res = poolboy:transaction(Pool,
+            fun(Conn) ->
+                {true, #{<<"result">> := Results}} = mongo:command(Conn,
+                    {<<"aggregate">>, Coll, <<"pipeline">>, [
+                        {<<"$match">>, Match},
+                        {<<"$group">>, Group}
+                    ]}),
+                Results
+            end),
+    {reply, {ok, Res}, State};
+
+handle_call({match_group, Coll, Match, Group, Sort}, _From, #state{pool=Pool} = State) ->
+    Res = poolboy:transaction(Pool,
+            fun(Conn) ->
+                {true, #{<<"result">> := Results}} = mongo:command(Conn,
+                    {<<"aggregate">>, Coll, <<"pipeline">>, [
+                        {<<"$match">>, Match},
+                        {<<"$group">>, Group},
+                        {<<"$sort">>, Sort}
+                    ]}),
+                Results
+            end),
+    {reply, {ok, Res}, State};
+
+handle_call({match_group, Coll, Match, Project, Group, Sort}, _From, #state{pool=Pool} = State) ->
+    Res = poolboy:transaction(Pool,
+            fun(Conn) ->
+                {true, #{<<"result">> := Results}} = mongo:command(Conn,
+                    {<<"aggregate">>, Coll, <<"pipeline">>, [
+                        {<<"$match">>, Match},
+                        {<<"$project">>, Project},
+                        {<<"$group">>, Group},
+                        {<<"$sort">>, Sort}
+                    ]}),
+                Results
+            end),
+    {reply, {ok, Res}, State};
+
+handle_call({delete, Coll, Match}, _From, #state{pool=Pool} = State) ->    
     Reply = poolboy:transaction(Pool,
             fun(Conn) ->
-                mongo:delete(Conn, Coll, Selector)
+                mongo:delete(Conn, Coll, Match)
             end),
     {reply, {ok, Reply}, State};
 
-handle_call({delete_one, Coll, Selector}, _From, #state{pool=Pool} = State) ->    
+handle_call({delete_one, Coll, Match}, _From, #state{pool=Pool} = State) ->    
     Reply = poolboy:transaction(Pool,
             fun(Conn) ->
-                mongo:delete_one(Conn, Coll, Selector)
+                mongo:delete_one(Conn, Coll, Match)
             end),
     {reply, {ok, Reply}, State};
 
-handle_call({count, Coll, Selector, Limit}, _From, #state{pool=Pool} = State) ->
+handle_call({count, Coll, Match, Limit}, _From, #state{pool=Pool} = State) ->
     Reply = poolboy:transaction(Pool,
             fun(Conn) ->
-                mongo:count(Conn, Coll, Selector, Limit)
+                mongo:count(Conn, Coll, Match, Limit)
             end),
     {reply, {ok, Reply}, State};
 
